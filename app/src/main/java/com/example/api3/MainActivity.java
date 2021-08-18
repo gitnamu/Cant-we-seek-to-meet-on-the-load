a663f1c68158538eca8b1fddeb86c0117ac0868f
jwPark6 <qkrwldn054@naver.com>
2021-08-18 21:42:54 +0900

    basic_structure

--- app/src/main/java/com/example/api3/MainActivity.java
+++ app/src/main/java/com/example/api3/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.api3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapPolyLine;
 import com.skt.Tmap.TMapView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +19,22 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
         TMapView tMapView = new TMapView(this);
-
         tMapView.setSKTMapApiKey( "l7xx3b3f893778d04343a7a2fbd0e5bb1599" );
+
+
+
+        TMapPoint tMapPointStart = new TMapPoint(37.570841, 126.985302);
+        TMapPoint tMapPointEnd = new TMapPoint(37.551135, 126.988205);
+
+        try {
+            TMapPolyLine tMapPolyLine = new TMapData().findPathData(tMapPointStart, tMapPointEnd);
+            tMapPolyLine.setLineColor(Color.BLUE);
+            tMapPolyLine.setLineWidth(2);
+            tMapView.addTMapPolyLine("Line1", tMapPolyLine);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
         linearLayoutTmap.addView( tMapView );
     }
-
-
 }
